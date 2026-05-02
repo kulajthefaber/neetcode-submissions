1683c9f05becf9d096da6a2eff852eb0b7db0de5
Kulaj Dasanayaka <kulajmandara7@gmail.com>
2026-05-02 20:45:11 +0530

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map = {}
+        output = []
+        for num in nums:
+            if num in map:
+                map[num] += 1
+            else:
+                map[num] = 1
+        
+        sorted_dict_desc = dict(sorted(map.items(), key=lambda item: item[1], reverse=True))
+
+        for i in range(k):
+            ithkey = list(sorted_dict_desc)[i]
+            output.append(ithkey)
+
+        return output
